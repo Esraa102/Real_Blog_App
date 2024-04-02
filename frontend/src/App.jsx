948f12c3b30a79434f3e42242b5bba8684a2cd34
Esraa102 <manalellban9@gmailcom>
2024-04-02 20:36:04 +0200

    install react js and tailwind css

---
+++ frontend/src/App.jsx
@@ -0,0 +1,15 @@
+import { Routes, Route } from "react-router-dom";
+import { Home } from "./pages";
+function App() {
+  return (
+    <>
+      <div>
+        <Routes>
+          <Route path="/" element={<Home />} />
+        </Routes>
+      </div>
+    </>
+  );
+}
+
+export default App;
