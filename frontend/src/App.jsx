0be0a2740827ccc63b3e59ffdfa59a043562588d
Esraa102 <manalellban9@gmailcom>
2024-04-02 20:46:40 +0200

    creating pages

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,13 +1,23 @@
 import { Routes, Route } from "react-router-dom";
-import { Home } from "./pages";
+import { Dashboard, Home, Login, PageError, Projects, Register } from "./pages";
+import { Header, Footer } from "./components";
+import { Toaster } from "react-hot-toast";
 function App() {
   return (
     <>
+      <Header />
       <div>
         <Routes>
           <Route path="/" element={<Home />} />
+          <Route path="/sign-up" element={<Register />} />
+          <Route path="/sign-in" element={<Login />} />
+          <Route path="/dashboard" element={<Dashboard />} />
+          <Route path="/projects" element={<Projects />} />
+          <Route path="*" element={<PageError />} />
         </Routes>
+        <Toaster />
       </div>
+      <Footer />
     </>
   );
 }
--- frontend/src/pages/index.js
+++ frontend/src/pages/index.js
@@ -1,3 +1,8 @@
 import Home from "./Home";
+import Register from "./Register";
+import Login from "./Login";
+import Projects from "./Projects";
+import Dashboard from "./Dashboard";
+import PageError from "./PageError";
 
-export { Home };
+export { Home, Register, Login, Projects, Dashboard, PageError };
