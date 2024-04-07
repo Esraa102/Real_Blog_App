fc0fdb5858074ea91dfa4308031aa23343c3011e
Esraa102 <manalellban9@gmailcom>
2024-04-07 11:00:14 +0200

    update functionaliy

--- frontend/src/components/LogOut.jsx
+++ frontend/src/components/LogOut.jsx
@@ -1,15 +1,19 @@
+/* eslint-disable react-hooks/exhaustive-deps */
 import { useLogOutUserMutation } from "../features/auth/api/authApiSlice";
 import toast from "react-hot-toast";
 import { useNavigate } from "react-router-dom";
 import { useDispatch } from "react-redux";
 import { logOutUserSuccess } from "../features/auth/authSlice";
-const LogOut = ({ full }) => {
+import { useEffect } from "react";
+const LogOut = () => {
   const [logOutUser, { data, isLoading, error, isSuccess }] =
     useLogOutUserMutation();
   const navigate = useNavigate();
   const dispatch = useDispatch();
   const logOut = () => {
     logOutUser();
+  };
+  useEffect(() => {
     if (isSuccess) {
       dispatch(logOutUserSuccess());
       toast.success(data);
@@ -18,7 +22,7 @@ const LogOut = ({ full }) => {
     if (error) {
       toast.error(error.message);
     }
-  };
+  }, [isSuccess, error]);
   return (
     <button
       type="button"
