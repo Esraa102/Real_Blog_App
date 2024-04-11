bd159de54c418924d6fb56a2ac826ee02c7557a3
Esraa102 <manalellban9@gmailcom>
2024-04-11 17:59:19 +0200

    update comment route

--- backend/controllers/comment.controller.js
+++ backend/controllers/comment.controller.js
@@ -106,11 +106,22 @@ const getAllComments = async (req, res, next) => {
     try {
       const limit = parseInt(req.query.limit) || 9;
       const startIndex = parseInt(req.query.startIndex) || 0;
+      const sortDirection = req.query.sort === "asc" ? 1 : -1;
       const comments = await Comment.find()
-        .sort({ createdAt: -1 })
+        .sort({ createdAt: sortDirection })
         .skip(startIndex)
         .limit(limit);
-      res.status(200).json({ comments });
+      const totalComments = await Comment.countDocuments();
+      const now = new Date();
+      const oneMonthAgo = new Date(
+        now.getFullYear(),
+        now.getMonth() - 1,
+        now.getDate()
+      );
+      const totalCommentsMonthAgo = await Comment.countDocuments({
+        createdAt: { $gte: oneMonthAgo },
+      });
+      res.status(200).json({ comments, totalComments, totalCommentsMonthAgo });
     } catch (error) {
       next(customError(res.status(500), error.message));
     }
